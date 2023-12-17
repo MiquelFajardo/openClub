2a552db4b3a19cc5c93963e63575ca9bc9bd65ef
Miquel Fajardo <miquel.fajardo@protonmail.com>
2023-12-17 12:06:44 +0100

    Implementació DAO de Provincia i testejat

--- src/test/informacioContacteTest/InformacioContacteTest.java
+++ src/test/informacioContacteTest/InformacioContacteTest.java
@@ -1,9 +1,7 @@
 package test.informacioContacteTest;
 
 
-import main.java.informacioContacte.poblacio.Pais;
-import main.java.informacioContacte.poblacio.PaisDAO;
-import main.java.informacioContacte.poblacio.PaisTaula;
+import main.java.informacioContacte.poblacio.*;
 import org.junit.jupiter.api.*;
 
 import java.sql.*;
@@ -26,15 +24,23 @@ public class InformacioContacteTest {
     private static Pais pais2;
     private static PaisDAO paisDAO;
 
+    private static Provincia provincia1;
+    private static Provincia provincia2;
+    private static ProvinciaDAO provinciaDAO;
+
     @BeforeAll
     public static void setupTest() throws SQLException {
         connexio = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
 
         PaisTaula.iniciar(connexio);
         paisDAO = new PaisDAO(connexio);
-
         pais1 = new Pais(1, "Espanya");
         pais2 = new Pais(2, "Portugal");
+
+        ProvinciaTaula.iniciar(connexio);
+        provinciaDAO = new ProvinciaDAO(connexio);
+        provincia1 = new Provincia(1, "Catalunya", 1);
+        provincia2 = new Provincia(2, "Andalusia", 1);
     }
 
     @Test
@@ -78,10 +84,60 @@ public class InformacioContacteTest {
 
     @Test
     @Order(4)
-    public void testObtenirTotsElsPaissos() {
+    public void testObtenirPaissos() {
         paisDAO.emmagatzemar(pais2);
         List<Pais> paissos = paisDAO.obtenirTot();
         assertEquals(2, paissos.size());
+    }
+
+    @Test
+    @Order(5)
+    public void testEmmagatzemarProvincia() throws SQLException {
+        boolean resultat = false;
+        provinciaDAO.emmagatzemar(provincia1);
+        String sentenciaSQL = "SELECT nom FROM provincia";
+        Statement statement = connexio.createStatement();
+        ResultSet resultSet = statement.executeQuery(sentenciaSQL);
+        while (resultSet.next()) {
+            resultat = true;
+            assertEquals(resultSet.getString("nom"), provincia1.getNom());
+        }
+        assertTrue(resultat);
+    }
+
+    @Test
+    @Order(6)
+    public void testModificarProvincia() throws SQLException {
+        boolean resultat = false;
+        String nomNou = "Algarve";
+        int nouIdPais = 2;
+        provincia1.setNom(nomNou);
+        provincia1.setIdPais(nouIdPais);
+        provinciaDAO.modificar(provincia1);
+        String sentenciaSQL = "SELECT nom, id_pais FROM provincia";
+        Statement statement = connexio.createStatement();
+        ResultSet resultSet = statement.executeQuery(sentenciaSQL);
+        while (resultSet.next()) {
+            resultat = true;
+            assertEquals(resultSet.getString("nom"), nomNou);
+            assertEquals(resultSet.getLong("id_pais"), nouIdPais);
+        }
+        assertTrue(resultat);
+    }
+
+    @Test
+    @Order(7)
+    public void testObtenirProvincia() {
+        Provincia provinciaObtinguda = provinciaDAO.obtenir(1L);
+        assertEquals(provinciaObtinguda.toString(), provincia1.toString());
+    }
+
+    @Test
+    @Order(8)
+    public void testObtenirProvincies() {
+        provinciaDAO.emmagatzemar(provincia2);
+        List<Provincia> provinces = provinciaDAO.obtenirTot();
+        assertEquals(2, provinces.size());
     }
 
     @AfterAll
